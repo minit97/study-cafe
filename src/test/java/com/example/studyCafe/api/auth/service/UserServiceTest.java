838eef96504722446fa47b5419660644dab5fd31
박현민 <dndb4902@naver.com>
2023-10-11 12:55:52 +0900

    # userServiceTest 성공케이스 완료

--- src/test/java/com/example/studyCafe/api/auth/service/UserServiceTest.java
+++ src/test/java/com/example/studyCafe/api/auth/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.example.studyCafe.api.auth.service;
 
 import com.example.studyCafe.api.auth.dto.SignupDto;
 import com.example.studyCafe.api.auth.dto.UserDto;
+import com.example.studyCafe.api.auth.model.Authority;
 import com.example.studyCafe.api.auth.model.User;
 import com.example.studyCafe.api.auth.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +26,10 @@ class UserServiceTest {
     private UserService userService;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private AuthenticationManagerBuilder authenticationManagerBuilder;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @BeforeEach
     void clean() {
@@ -26,7 +38,7 @@ class UserServiceTest {
 
     @Test
     @DisplayName("회원 가입 - 성공")
-    void signup() {
+    void signupSuccess() {
         // given
         SignupDto signupDto = SignupDto.builder()
                 .username("phm")
@@ -46,44 +58,54 @@ class UserServiceTest {
 
     @Test
     @DisplayName("본인 조회 - 성공")
-    void my_search() {
+    void myInfoSuccess() {
         // given
-        SignupDto signupDto = SignupDto.builder()
-                .username("phm")
-                .password("123")
-                .nickname("박현민")
+        User user = User.builder()
+                .username("test")
+                .password(passwordEncoder.encode("1234"))
+                .nickname("tester")
+                .authorities(Set.of(new Authority("ROLE_USER")))
                 .build();
-        userService.signup(signupDto);
+        userRepository.save(user);
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken("test", "1234");
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
         // when
-        UserDto user = userService.getUserWithAuthorities("phm");
+        UserDto resultUser = userService.getMyUserWithAuthorities();
 
         // then
-        assertNotNull(user);
-        assertEquals(1L, userRepository.count());
-        assertEquals("phm", user.getUsername());
-        assertEquals("박현민", user.getNickname());
+        assertEquals("test", resultUser.getUsername());
+        assertEquals("tester", resultUser.getNickname());
+        boolean hasUserRole = resultUser.getAuthorityDtoSet()
+                .stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthorityName().equals("ROLE_USER"));
+        assertTrue(hasUserRole);
     }
 
     @Test
     @DisplayName("관리자 전용 user 1명 조회 - 성공")
     void admin_user_search() {
         // given
-        SignupDto signupDto = SignupDto.builder()
-                .username("phm")
-                .password("123")
-                .nickname("박현민")
+        User user = User.builder()
+                .username("test")
+                .password(passwordEncoder.encode("1234"))
+                .nickname("tester")
+                .authorities(Set.of(new Authority("ROLE_USER")))
                 .build();
-        userService.signup(signupDto);
+        userRepository.save(user);
 
         // when
-        UserDto user = userService.getUserWithAuthorities("phm");
+        UserDto search = userService.getUserWithAuthorities("test");
 
         // then
-        assertNotNull(user);
-        assertEquals(1L, userRepository.count());
-        assertEquals("phm", user.getUsername());
-        assertEquals("박현민", user.getNickname());
+        assertEquals("test", search.getUsername());
+        assertEquals("tester", search.getNickname());
+        boolean hasUserRole = search.getAuthorityDtoSet()
+                .stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthorityName().equals("ROLE_USER"));
+        assertTrue(hasUserRole);
     }
 
 }
